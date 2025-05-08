f57c3dd12468d47dce954e1878a517c40e04b920
asnaweevixvify <asnaweeezor@gmail.com>
2025-05-09 06:11:51 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Cal from './Cal'
+
+function App() {
+
+  return (
+    <>
+      <h1>Calculator</h1>
+      <Cal />
+    </>
+  )
+}
+
+export default App
---
+++ src/Cal.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import './App.css'
+import { sqrt } from 'mathjs'
+import { create, all } from 'mathjs'
+
+function Cal(){
+    const[displayValue,setDisplayValue] = useState(0)
+    const config = { }
+    const math = create(all, config)
+    return(
+        <>
+        <input type='text' readOnly className='display' value={displayValue}></input>
+        <div className='cal-container'>
+            <div className="col1">
+                <span onClick={() => showDisplay('7')}>7</span>
+                <span onClick={() => showDisplay('4')}>4</span>
+                <span onClick={() => showDisplay('1')}>1</span>
+                <span onClick={() => showDisplay('0')}>0</span>
+            </div>
+            <div className="col2">
+                <span onClick={() => showDisplay('8')}>8</span>
+                <span onClick={() => showDisplay('5')}>5</span>
+                <span onClick={() => showDisplay('2')}>2</span>
+                <span onClick={() => showDisplay('.')}>.</span>
+            </div>
+            <div className="col3">
+                <span onClick={() => showDisplay('9')}>9</span>
+                <span onClick={() => showDisplay('6')}>6</span>
+                <span onClick={() => showDisplay('3')}>3</span>
+                <span onClick={() => showDisplay('=')}>=</span>
+            </div>
+            <div className="col4">
+                <span onClick={() => showDisplay('/')}>/</span>
+                <span onClick={() => showDisplay('*')}>x</span>
+                <span onClick={() => showDisplay('-')}>-</span>
+                <span onClick={() => showDisplay('+')}>+</span>
+            </div>
+        </div>
+        </>
+    )
+
+    function showDisplay(newNum){
+        if (displayValue === 0) {
+            setDisplayValue(newNum);
+        }
+        else if(newNum === '='){
+            try{
+                setDisplayValue(math.evaluate(displayValue))
+            }
+            catch(err){
+                setDisplayValue('error')
+            }
+        } else {
+            setDisplayValue(displayValue.toString()+newNum)
+        }
+
+    }
+
+
+}
+
+export default Cal
